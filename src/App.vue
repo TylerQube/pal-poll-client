08589ac4efee186bf88f549996262acc293f4ad9
BuildTools <tylersmith1308@gmail.com>
2022-06-12 22:27:53 -0500

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,37 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <PollLogin> </PollLogin>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts">
+import { defineComponent } from "vue";
+import PollLogin from "./components/PollLogin.vue";
 
-export default {
-  name: 'App',
+export default defineComponent({
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    PollLogin,
+  },
+});
 </script>
 
-<style>
+<style lang="scss">
+html, body {
+  height: 100%;
+  margin: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  height: 100%;
 }
 </style>
---
+++ src/components/PollLogin.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="container">
+    <p class="login-header">Login</p>
+    <v-form>
+        <!-- <v-text-field 
+          v-model="username" 
+          label="Username" 
+        ></v-text-field> -->
+        <v-text-field 
+          type="password" 
+          v-model="password" 
+          label="Password"
+          prefix=""
+        ></v-text-field>
+        <button :disabled="btnDisabled" @click="login">LOG IN</button>
+    </v-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+        username: "",
+        password: ""
+    }
+  },
+  computed: {
+    btnDisabled() {
+        return this.username === "" || this.password === "";
+    }
+  },
+  methods: {
+      login() {
+          console.log("logging in...");
+      }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+.login-header {
+  color: black;
+  font-size: 2em;
+  font-weight: 400;
+}
+
+.container {
+  h1, h2, h3 {
+      color: black;
+  }
+  font-family: 'Abel', sans-serif;
+  border-radius: 0.2em;
+  padding: 0.5em 1em;
+
+  min-width: 25%;
+
+  border: 1px solid rgb(214, 214, 214);
+  // box-shadow: 3px 3px 8px 1px rgba(219,219,219,0.8);
+}
+
+form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+button {
+    width: 100%;
+    background-color: rgb(30, 58, 151);
+    color: white;
+    padding: 0.7em;
+    border: none;
+    font-size: 1em;
+
+    &:disabled {
+        background-color: rgb(156, 156, 156);
+    }
+}
+</style>

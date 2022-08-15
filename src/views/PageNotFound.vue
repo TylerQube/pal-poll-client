b7b23e6198463f9f380e255742fccf55c0a9fa1f
BuildTools <tylersmith1308@gmail.com>
2022-08-14 19:08:48 -0500

    Add Error 404 View

---
+++ src/views/PageNotFound.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container shadow">
+    <h1>Sorry, I couldn't find that page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+    .container {
+        width: 100%;
+        height: calc(100vh - 60px);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
